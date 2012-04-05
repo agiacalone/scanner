5058a9cc108e6bc1484b6a9f3c921e439343f0de
Anthony Giacalone <agiacalone@gmail.com>
2012-04-05 10:46:30 -0700

    significantly reduced number of states in the tables

--- codes.py
+++ codes.py
@@ -1,5 +1,5 @@
 class codes:
-    lookup_codes = ["!accept", "grave", "leftbracket", "rightbracket", "leftbrace", "rightbrace", "leftsquig", "rightsquig", "atsymbol", "ampersand", "pound", "bang", "tilde", "singlequote", "dollarsign", "colon", "semicolon", "period", "comma", "plus", "minus", "slash", "backslash", "star", "equal", "carat", "leftparen", "rightparen", "id", "integer", "space", "dless", "spaceship", "lesseq", "dgreater", "greatereq", "dbang", "noteq", "coloneq", "fixed", "dplus", "plusminus", "minusplus", "ddash", "slasheq", "eqeq", "string", "comment", "currency", "file", "library", "scinotation"]
+    lookup_codes = ["!accept", "grave", "leftbracket", "rightbracket", "leftbrace", "rightbrace", "leftsquig", "rightsquig", "atsymbol", "ampersand", "pound", "bang", "tilde", "singlequote", "dollarsign", "colon", "semicolon", "period", "comma", "plus", "minus", "slash", "backslash", "star", "equal", "carat", "leftparen", "rightparen", "id", "integer", "space", "dless", "spaceship", "lesseq", "dgreater", "greatereq", "dbang", "noteq", "coloneq", "dplus", "plusminus", "minusplus", "ddash", "slasheq", "eqeq", "fixed", "string", "comment", "currency", "file", "library", "scinotation"]
     
     def getcode(code):
         return codes.lookup_codes[code]
