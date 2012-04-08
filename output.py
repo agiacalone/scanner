099b55ea97e6c7041231d79092de71541d7dad82
Anthony Giacalone <agiacalone@gmail.com>
2012-04-08 14:59:29 -0700

    changed id table to dictionary and added output of dictionary to the gui

--- output.py
+++ output.py
@@ -7,3 +7,10 @@ class output:
         
     def output_gui(output_message, output_title, output_text):
         return textbox(msg=output_message, title=output_title, text=output_text, codebox=0)
+        
+    def output_dict(output_message, output_title, dictionary):
+        pretty_dict = 'Count' + '\t' + 'ID' + '\n\n'
+        for k, v in sorted(dictionary.items(), key=lambda count: count[1], reverse=True):
+            pretty_dict += (str(v) + '\t' + str(k) + '\n')
+        print(pretty_dict)
+        return textbox(msg=output_message, title=output_title, text=pretty_dict, codebox=0)
